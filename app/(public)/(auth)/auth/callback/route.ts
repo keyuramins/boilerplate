98c7b520f18f24b7ee9bf6cb4eebe70891d08ece
sidmaz666 <sidmazumder8@gmail.com>
2025-05-10 13:38:21 +0530

    fix: redirect to dashboard on login

--- app/(public)/(auth)/auth/callback/route.ts
+++ app/(public)/(auth)/auth/callback/route.ts
@@ -12,5 +12,5 @@ export async function GET(request: NextRequest) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(new URL(next || '/', request.url));
+  return NextResponse.redirect(new URL(next || '/dashboard', request.url));
 }
